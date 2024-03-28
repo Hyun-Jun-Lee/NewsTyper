9116ab31d9abd23bac310d2b9fea34e5a6272299
Lee Hyun Jun <bhk0827@gmail.com>
2024-03-29 05:54:23 +0900

    <Feat> : Add parse_html

--- src/news_crawler/base.py
+++ src/news_crawler/base.py
@@ -1,10 +1,7 @@
 import requests
-import asyncio
-import aiohttp
-
 from bs4 import BeautifulSoup
 
-from enums.enums import NewsAgency, YnaCategory
+from enums.enums import NewsAgency
 
 
 class BaseCrawler:
@@ -16,21 +13,7 @@ class BaseCrawler:
         else:
             raise ValueError(f"Wrong Agency : {agency}")
 
-    async def async_get_html(self, url: str):
-        """
-        aiohttp 라이브러리를 이용해 주어진 url의 html을 비동기적으로 가져오는 메서드
-        """
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as response:
-                    response.raise_for_status()  # 오류 발생 시 예외를 발생시킴
-                    html = await response.text()  # 응답의 텍스트(HTML)를 가져옴
-                    return html
-        except aiohttp.ClientError as e:
-            print(f"Error while crawling {url} : {e}")
-            return None
-
-    def get_html(self, url):
+    def parse_html(self, url):
         """
         requests 라이브러리를 이용해 주어진 url의 html을 가져오는 메서드
         """
@@ -38,19 +21,19 @@ class BaseCrawler:
         try:
             response = requests.get(url)
             response.raise_for_status()
-            return response.text
+            html = response.text
+            soup = BeautifulSoup(html, "html.parser")
+            return soup
+
         except requests.RequestException as e:
             print("Error while crawling {url} : {e}")
             return None
 
-    async def _crawl_article_links(self, category):
-        pass
-
-    def get_article_links(self, category: str):
+    def get_title_url(self, category: str):
         """
         사용자가 선택한 category의 기사 제목과 url 크롤링
         """
-        return asyncio.run(self._crawl_article_links(category))
+        pass
 
     def get_article_content(self, article_url):
         """
