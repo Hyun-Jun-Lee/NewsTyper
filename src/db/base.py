26e5b1111f008b6224b1eacae97aec6943de66e2
Lee Hyun Jun <bhk0827@gmail.com>
2024-06-10 02:12:47 +0900

    <Feat> : Set db setting

---
+++ src/db/base.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import as_declarative
+
+
+@as_declarative()
+class ModelBase:
+    __name__: str
+    __abstract__ = True
+    id = Column(Integer, primary_key=True, index=True)
+    created_time = Column(DateTime, default=func.now(), nullable=True)
+    modified_time = Column(DateTime, default=None, onupdate=func.now(), nullable=True)
+
+    # Generate __tablename__ auto
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    def to_dict(self):
+        return {
+            column.name: getattr(self, column.name) for column in self.__table__.columns
+        }
