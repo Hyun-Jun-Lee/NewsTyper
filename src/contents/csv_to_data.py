259700538ee5c4cbcd9aebae13410a3c01709941
Lee Hyun Jun <bhk0827@gmail.com>
2024-06-11 11:12:15 +0900

    <Fix> : append 옵션이 truncate가아니라 drop & create

--- src/contents/csv_to_data.py
+++ src/contents/csv_to_data.py
@@ -9,41 +9,28 @@ movie_quote_file_path = data_folder / "movie_quotes.csv"
 
 def get_data():
     all_quotes_df = pd.read_csv(all_quotes_file_path, usecols=["Quote", "Author"])
+
     anime_quote_df = pd.read_csv(
         anime_quote_file_path, usecols=["Quote", "Character", "Anime"]
     )
+    anime_quote_df["Author"] = anime_quote_df.apply(
+        lambda row: f"{row['Character']} ({row['Anime']})", axis=1
+    )
+    anime_quote_data = anime_quote_df[["Quote", "Author"]]
+
     movie_quote_df = pd.read_csv(
         movie_quote_file_path, usecols=["quote", "movie", "year"]
     )
-
-    all_quote_data = all_quotes_df[["Quote", "Author"]]
-
-    # 컬럼 합치기
-    anime_quote_data = anime_quote_df[["Quote", "Character"]]
-    anime_quote_data["Character"] = anime_quote_df.apply(
-        lambda row: f"{row['Character']} ({row['Anime']})", axis=1
-    )
-
-    movie_quote_data = movie_quote_df[["quote", "movie"]]
-    movie_quote_data["movie"] = movie_quote_df.apply(
+    movie_quote_df["Author"] = movie_quote_df.apply(
         lambda row: f"{row['movie']} ({row['year']})", axis=1
     )
-
-    # 컬럼 이름 변경
-    all_quote_data.rename(
-        columns={"Quote": "en_content", "Character": "author"}, inplace=True
-    )
-    anime_quote_data.rename(
-        columns={"Quote": "en_content", "Character": "author"}, inplace=True
-    )
-    movie_quote_data.rename(
-        columns={"quote": "en_content", "movie": "author"}, inplace=True
-    )
+    movie_quote_df = movie_quote_df.rename(columns={"quote": "Quote"})
+    movie_quote_data = movie_quote_df[["Quote", "Author"]]
 
     # DataFrame 병합
     combined_data = pd.concat(
-        [all_quote_data, anime_quote_data, movie_quote_data], ignore_index=True
-    )
+        [all_quotes_df, anime_quote_data, movie_quote_data], ignore_index=True
+    ).rename(columns={"Quote": "en_content", "Author": "author"})
     combined_data["kr_content"] = None
     combined_data["is_custom"] = False
     return combined_data
